5aedb81b9b01b333b82b59927a94e0fa355bc175
SOEUN KWON <thdms5128@gmail.com>
2022-03-02 16:03:03 +0900

    input 파일에서 string으로 한 줄씩 받아오고 출력 확인

--- EmployeeManagementSystem/EmployeeManagementSystem/main.cpp
+++ EmployeeManagementSystem/EmployeeManagementSystem/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-
-
-
+using namespace std;
 
 int main(int argc, char** argv) {
+	ifstream inputFile;
+	inputFile.open("input.txt");
+
+	if (inputFile.is_open()) {
+		string str;
+		while (getline(inputFile, str)) {
+			cout << str << endl;
+		}
+
+	}
+
+	inputFile.close();
 
 	return 0;
 }
