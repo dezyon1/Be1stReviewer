357d20909bb031984790010eb17444b1fe234727
Hoseong Kang <karosung@gmail.com>
2022-03-04 08:54:19 +0900

    [Feature] Database 멤버 변수의 리턴 타입 변경

--- EmployeeManagementSystem/EmployeeManagementSystem/Database.h
+++ EmployeeManagementSystem/EmployeeManagementSystem/Database.h
@@ -11,21 +11,28 @@ template <typename record>
 class IDatabase {
 public:
 	virtual void add(record& data) = 0;
-	virtual string del(string const& column, string const& keyword) = 0;
-	virtual string sch(string const& column, string const& keyword) = 0;
-	virtual string mod(string const& column, string const& keyword,
+	virtual vector<record>& del(string const& column, string const& keyword) = 0;
+	virtual vector<record>& sch(string const& column, string const& keyword) = 0;
+	virtual vector<record>& mod(string const& column, string const& keyword,
 		string const& tarColumn, string const& tarKeyword) = 0;
 };
 
 template <typename record>
-class Database : public IDatabase<record> {
+class VectorDatabase : public IDatabase<record> {
 public:
 	virtual void add(record& data) override {};
-	virtual string del(string const& column, string const& keyword) override { return ""; };
-	virtual string sch(string const& column, string const& keyword) override { return ""; };
-	virtual string mod(string const& column, string const& keyword,
+	virtual vector<record>& del(string const& column, string const& keyword) override {
+		vector<record> temp;
+		return temp;
+	};
+	virtual vector<record>& sch(string const& column, string const& keyword) override {
+		vector<record> temp;
+		return temp;
+	};
+	virtual vector<record>& mod(string const& column, string const& keyword,
 		string const& tarColumn, string const& tarKeyword) override {
-		return "";
+		vector<record> temp;
+		return temp;
 	};
 
 private:
