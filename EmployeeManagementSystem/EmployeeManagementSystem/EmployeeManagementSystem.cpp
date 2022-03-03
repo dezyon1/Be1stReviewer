ecb098fdddc8485e3cff062fa57787ea988200ba
Jongseok Kim <dezyon1@gmail.com>
2022-03-03 16:21:42 +0900

    [Refactoring] EmployManagementSystem 구조 변경 및 main함수 수정
    EmployManagementSystem의 interface를 재정의하고
    해당 변경에 따른 main함수 및 테스트 케이스 수정함

--- EmployeeManagementSystem/EmployeeManagementSystem/EmployeeManagementSystem.cpp
+++ EmployeeManagementSystem/EmployeeManagementSystem/EmployeeManagementSystem.cpp
@@ -2,39 +2,48 @@
 #include <string>
 #include "EmployeeManagementSystem.h"
 #include "employee.h"
+#include "input_validation.h"
 
 using namespace std;
 
-void EmployeeManagementSystem::inputCommand(string command)
+string EmployeeManagementSystem::runCommand(string command)
 {
+	string result;
+	if (!isValidInput(command))
+		throw invalid_argument("입력 형식에 오류가 있습니다: " + command);
+
+	if (command.find_first_of("ADD") == 0) {
+
+	}
+
+	if (command.find_first_of("DEL") == 0) {
+
+	}
+
+	if (command.find_first_of("MOD") == 0) {
+
+	}
+
+	if (command.find_first_of("SCH") == 0) {
+
+	}
+
+
+	return result;
 }
 
-void EmployeeManagementSystem::add(Employee* employee)
-{
-	cout << "To do implement add fuction" << endl;
+string EmployeeManagementSystem::add(string command) {
+	return "";
 }
 
-void EmployeeManagementSystem::del(Employee* employee)
-{
-	cout << "To do implement del fuction" << endl;
+string EmployeeManagementSystem::del(string command) {
+	return "";
 }
 
-void EmployeeManagementSystem::sch(Employee* employee)
-{
-	cout << "To do implement sch fuction" << endl;
+string EmployeeManagementSystem::mod(string command) {
+	return "";
 }
 
-void EmployeeManagementSystem::mod(Employee* employee)
-{
-	cout << "To do implement mod fuction" << endl;
-}
-
-void EmployeeManagementSystem::printResult()
-{
-	cout << getResultStr();
-}
-
-string EmployeeManagementSystem::getResultStr()
-{
+string EmployeeManagementSystem::sch(string command) {
 	return "";
 }
