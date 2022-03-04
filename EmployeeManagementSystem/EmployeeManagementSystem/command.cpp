0d1a5aaa5df25de72715edef2f3ae65837559cda
dhin131 <dhin131@gmail.com>
2022-03-04 14:45:01 +0900

    AddCommand 부모 클래스에서 저장한 token을 사용

--- EmployeeManagementSystem/EmployeeManagementSystem/command.cpp
+++ EmployeeManagementSystem/EmployeeManagementSystem/command.cpp
@@ -3,27 +3,14 @@
 
 EmployeeInfo AddCommand::getEmployeeInfo(string command)
 {
-	string info = command.substr(command.find(',', InfoStartIndex) + 1, command.length() - 1);
-	int sequence = 0;
-	int previous = 0;
-	int current = info.find(',');
-
-	while (current != string::npos)
-	{
-		string substring = info.substr(previous, current - previous);
-		employeeInfoSeq[sequence++] = substring;
-		previous = current + 1;
-		current = info.find(',', previous);
-	}
-	employeeInfoSeq[sequence] = info.substr(previous, current - previous);
-
 	EmployeeInfo employeeInfo;
-	employeeInfo.employeeNumber = employeeInfoSeq[EmployeeNum];
-	employeeInfo.name = employeeInfoSeq[Name];
-	employeeInfo.cl = employeeInfoSeq[CL];
-	employeeInfo.phoneNumber = employeeInfoSeq[PhoneNum];
-	employeeInfo.birth = employeeInfoSeq[Birthday];
-	employeeInfo.certi = employeeInfoSeq[CERTI];
+
+	employeeInfo.employeeNumber = commandTokens[4];
+	employeeInfo.name = commandTokens[5];
+	employeeInfo.cl = commandTokens[6];
+	employeeInfo.phoneNumber = commandTokens[7];
+	employeeInfo.birth = commandTokens[8];
+	employeeInfo.certi = commandTokens[9];
 
 	return employeeInfo;
 }
